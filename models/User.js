319d4b0d48a5d960941eb2af3ff0695080f71f4c
Lester Lee <ll6@williams.edu>
2019-04-04 18:51:02 -0400

    Pass data to handlebars correctly

--- models/User.js
+++ models/User.js
@@ -5,12 +5,13 @@ User.add({
   displayName: {type: String},
   password: {type: keystone.Field.Types.Password},
   email: {type: keystone.Field.Types.Email, unique: true},
+  isAdmin: {type: Boolean, label: 'Can access Keystone'},
 });
 
 User.schema.virtual('canAccessKeystone').get(function(){
+  //return this.isAdmin;
   return true;
 });
 
-User.defaultColumns = 'id, displayName, email';
-
+User.defaultColumns = 'displayName, email, isAdmin';
 User.register();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var keystone = require('keystone');
 var importRoutes = keystone.importer(__dirname);
+
+keystone.set('404', function(req, res, next){
+  res.status(404).render('errors/404');
+});
+
 var routes = {
   views: importRoutes('./views'),
 };
 
 exports = module.exports = function(app){
-  app.get('/', routes.views.index)
+  app.get('/', routes.views.index);
+  app.get('/:page', routes.views.page);
 };
