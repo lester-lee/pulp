06a1559e8700bbff1a656ff6bc242e6b4be74c87
Lester Lee <ll6@williams.edu>
2019-04-04 11:59:13 -0400

    Get handlebars engine working

---
+++ keystone.js
@@ -0,0 +1,22 @@
+var keystone = require('keystone');
+var engine = require('express-handlebars');
+
+keystone.init({
+	'cookie secret': 'secure string goes here',
+	'name': 'pulp',
+	'user model': 'User',
+	'auto update': true,
+	'auth': true,
+	'views': 'views',
+	'custom engine': engine({
+		layoutsDir: 'views/layouts',
+		defaultLayout: 'main',
+	}),
+	'view engine': 'handlebars',
+});
+
+keystone.import('models');
+
+keystone.set('routes', require('./routes'));
+
+keystone.start();
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var keystone = require('keystone');
+var importRoutes = keystone.importer(__dirname);
+var routes = {
+  views: importRoutes('./views'),
+};
+
+exports = module.exports = function(app){
+  app.get('/', routes.views.index)
+};
